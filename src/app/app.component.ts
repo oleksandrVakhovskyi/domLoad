fb20c9a5d0ae04640b2f87d5c3ebe79e3b70dd40
oleksandrVakhovskyi <o.vakhovskyi@do-it.co>
2022-02-03 15:13:34 +0200

    domload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, iif, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'domload';
+
+  choiceSub!: Subscription;
+  documentLoad = fromEvent(document, 'DOMContentLoaded');
+  choice = iif(
+    () => !!this.documentLoad,
+    of('document has load'),
+    of('document was not loaded')
+  );
+
+  ngOnInit(){
+     this.createStream(); 
+  }
+
+  createStream(){
+    this.choiceSub = this.choice.pipe(
+      ).subscribe((i)=> { console.log('result: ', i);
+      }) 
+  }
+
+  ngOnDestroy(){
+    this.choiceSub.unsubscribe();
+  }
+
+
 }
